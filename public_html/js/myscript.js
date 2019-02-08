5968eb7ffdc5c8299d234036fc381140f4b83c9c
alexm <alexm@192.168.4.22>
2019-02-08 17:38:57 +0100

    Formulario principal

--- public_html/js/myscript.js
+++ public_html/js/myscript.js
@@ -1,4 +1,24 @@
-function paginaCargada(){
-    alert("Pagina cargada");
+function paginaCargada() {
+
+    var nombreFormulario = document.getElementById("inGeneradorFormulario");
+    var btnGeneraFormulario = document.getElementById("btnGeneradorFormulario");
+    var divContenedor = document.getElementById("contenedor");
+
+    btnGeneraFormulario.addEventListener('click', generarFormulario);
+
+    function generarFormulario() {
+        if (nombreFormulario.value != '') {
+            var div = document.createElement('div');
+            div.setAttribute('id', nombreFormulario.value);
+            divContenedor.appendChild(div);
+        }else{
+            alert("debes darle un nombre al formulario");
+        }
+
+    }
+
 }
+
+
+
 
