6e7ed21081071c108f9dab5dd3fa52a226eb1b5f
alexm <alexm@192.168.4.22>
2019-02-11 21:25:22 +0100

    Implementado borrar formulario

--- public_html/js/myscript.js
+++ public_html/js/myscript.js
@@ -9,24 +9,44 @@ function paginaCargada() {
     function generarFormulario() {
         if (nombreFormulario.value != '') {
             var div = document.createElement('div');
+            var form = document.createElement('div');
             var btnEditarFormulario = document.createElement('button');
             var btnBorrarFormulario = document.createElement('button');
+            var tituloFormulario = document.createElement('h5');
+            var contenidoEditar = document.createTextNode('Editar Formulario');
+            var contenidoBorrar = document.createTextNode('Borrar Formulario');
+            var contenidoTitulo = document.createTextNode(nombreFormulario.value);
 
-            btnEditarFormulario.setAttribute('id', 'editarFormulario' + nombreFormulario.value);
-            var contenidoEditar = document.createTextNode('EditarFormulario');
+            btnEditarFormulario.setAttribute('id', nombreFormulario.value + '-editarFormulario');
             btnEditarFormulario.appendChild(contenidoEditar);
-            btnBorrarFormulario.setAttribute('id', 'borrarFormulario' + nombreFormulario.value);
-            var contenidoBorrar = document.createTextNode('BorrarFormulario');
+            btnBorrarFormulario.setAttribute('id', nombreFormulario.value + '-borrarFormulario');
             btnBorrarFormulario.appendChild(contenidoBorrar);
+            tituloFormulario.appendChild(contenidoTitulo);
 
 
-            div.setAttribute('id', nombreFormulario.value+'container');
+            form.setAttribute('id', nombreFormulario.value + '-form');
+            div.setAttribute('id', nombreFormulario.value + '-container');
+            div.appendChild(tituloFormulario);
             div.appendChild(btnEditarFormulario);
             div.appendChild(btnBorrarFormulario);
+            div.appendChild(form);
             divContenedor.appendChild(div);
 
+            btnEditarFormulario.addEventListener('click', diseñaFormulario);
+            btnBorrarFormulario.addEventListener('click', borraFormulario);
         } else {
             alert("debes darle un nombre al formulario");
+        }
+
+        function diseñaFormulario() {
+            var idForm = this.id.split('-', 2);
+            alert(idForm[0]);
+        }
+
+        function borraFormulario() {
+            var idForm = this.id.split('-', 2);
+            document.getElementById('contenedor').removeChild(document.getElementById(idForm[0]+'-container'));
+            
         }
 
     }
